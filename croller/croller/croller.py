39714e0d3bcd64d3543713d3b92c8dae96f39e37
AzureAD\SierraLee <sierra.lee@brtnx.com>
2022-11-04 10:19:44 +0900

    add tor

--- croller/croller/croller.py
+++ croller/croller/croller.py
@@ -17,22 +17,22 @@ df = pd.read_excel('./list.xlsx')
 modelCodeList = df['판매자상품코드'].tolist()
 option = Options()
 # option.add_argument("--proxy-server=socks5://127.0.0.1:9150")
-option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
-option.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+# option.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
+# option.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
 # option.add_argument("disable-infobars")
 # option.add_argument("disable-extensions")
 # #option.add_argument("start-maximized")
 # option.add_argument('disable-gpu')
 # option.add_argument('headless')
-
+# torexe = os.popen(r'')
 driver = webdriver.Chrome(r"C:\chromedriver.exe", options=option)
 
 
 driver.implicitly_wait(3)
 driver.get('http://www.enuri.com/')
-sleep(2)
-WebDriverWait(driver, 10).until(
-  EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="header-sr"]/div[1]"""))
+sleep(5)
+WebDriverWait(driver, 1000).until(
+  EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="search_keyword"]"""))
 )
 
 elm_search = driver.find_element(By.XPATH, """//*[@id="search_keyword"]""")
@@ -57,7 +57,7 @@ btn_search = driver.find_element(By.XPATH, """//*[@id="header-sr"]/div[1]/span[2
 btn_search.click()
 
 
-WebDriverWait(driver, 10).until(
+WebDriverWait(driver, 1000).until(
   EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="listBodyDiv"]/div[2]/div[1]/div[3]/div[1]"""))
 )
 # prices = soup.select('goods-cont type--list')
@@ -68,10 +68,10 @@ prices1 = soup.select('.tx--price')
 prices2 = soup.select('.col--price>a>em')
 prices = prices1+ prices2
 
-print(type(prices2))
+# print(type(prices2))
 
-# print(len(prices))
-# for p in prices:
-#   print(p.text)
+print(len(prices))
+for p in prices:
+  print(p.text)
 
 
