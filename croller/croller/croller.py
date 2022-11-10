cf06b3cadde706731b38123571da287e4d8b6bdc
AzureAD\SierraLee <sierra.lee@brtnx.com>
2022-11-10 09:32:47 +0900

    click include card discount

--- croller/croller/croller.py
+++ croller/croller/croller.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 from bs4 import BeautifulSoup
 import pandas as pd
 import urllib.request 
@@ -23,10 +24,10 @@ option = Options()
 # option.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36")
 # torexe = os.popen(r'')
 #*headless 상태에서 checkbox click을 도와줌
-option.add_argument("window-size=1920,1080")
-#*브라우저 키지 않고 작동
-option.add_argument('headless')
-option.add_argument("start-maximized")
+# option.add_argument("window-size=1920,1080")
+# #*브라우저 키지 않고 작동
+# option.add_argument('headless')
+# option.add_argument("start-maximized")
 # option.add_argument("disable-infobars")
 # option.add_argument("disable-extensions")
 # option.add_argument('disable-gpu')
@@ -46,71 +47,85 @@ modelCodeList = df['판매자상품코드'].tolist()
 # print(modelCodeList)
 
 #*상품코드별 최저가 검색 후 Data set 만들기
-sleep(5)
+sleep(0.3)
 WebDriverWait(driver, 1000).until(
   EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="search_keyword"]"""))
 )
 
 
 
-# elm_search = driver.find_element(By.XPATH, """//*[@id="search_keyword"]""")
-# # data.append([modelCodeList[i]])
-# elm_search.send_keys(modelCodeList[0])
-# btn_search = driver.find_element(By.XPATH, """//*[@id="header-sr"]/div[1]/span[2]""")
-# btn_search.click() 
-# sleep(0.3)
-# checkbox_rental = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[2]/div[2]/div/div[1]/ul/li[1]/label')
-# checkbox_rental.click()
-# checkbox_delivery = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[2]/div[2]/div/div[1]/ul/li[2]/label')
-# checkbox_delivery.click()
-# sleep(500)
+elm_search = driver.find_element(By.XPATH, """//*[@id="search_keyword"]""")
+# data.append([modelCodeList[i]])
+elm_search.send_keys(modelCodeList[0])
+btn_search = driver.find_element(By.XPATH, """//*[@id="header-sr"]/div[1]/span[2]""")
+btn_search.click() 
+sleep(0.3)
+first_product = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[3]/div[1]/ul/li[1]/div/div[1]')
+first_product.click()
+sleep(0.3)
 
-data = []
-print(len(modelCodeList))
-#* for문 여기서 부터 시작
-for i in range(len(modelCodeList)):
-  elm_search = driver.find_element(By.XPATH, """//*[@id="search_keyword"]""")
-  data.append([modelCodeList[i]])
-  elm_search.send_keys(modelCodeList[i])
+driver.switch_to.window(driver.window_handles[1])
+btn_card_discount = driver.find_element(By.XPATH, '//*[@id="prod_pricecomp"]/div[1]/div/div[2]/div[2]/label')
+btn_card_discount.click()
+sleep(0.2)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+print(html)
+# price = soup.select('.comparison__rt>.cont__box>.comprod__list>.comprod__item>.col.col--rt>.pinfo__group>.line__price>a>em')
+# for p in price:
+#   print(p.text)
+sleep(500)
 
-  btn_search = driver.find_element(By.XPATH, """//*[@id="header-sr"]/div[1]/span[2]""")
-  btn_search.click() 
+# data = []
+# print(len(modelCodeList))
+# #* for문 여기서 부터 시작
+# for i in range(len(modelCodeList)):
+#   data.append([modelCodeList[i]])
+#   elm_search = driver.find_element(By.XPATH, """//*[@id="search_keyword"]""")
+#   elm_search.send_keys(modelCodeList[i])
 
-  # WebDriverWait(driver, 1000).until(
-  #   EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="listBodyDiv"]/div[2]/div[1]/div[3]/div[1]"""))
-  # )
+#   btn_search = driver.find_element(By.XPATH, """//*[@id="header-sr"]/div[1]/span[2]""")
+#   btn_search.click() 
 
-  sleep(0.3)
-  checkbox_rental = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[2]/div[2]/div/div[1]/ul/li[1]/label')
-  checkbox_rental.click()
-  checkbox_delivery = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[2]/div[2]/div/div[1]/ul/li[2]/label')
-  checkbox_delivery.click()
-  sleep(0.2)
-  html = driver.page_source
-  soup = BeautifulSoup(html, 'html.parser')
-  prices1 = soup.select('.tx--price')
-  prices2 = soup.select('.col--price>a>em')
-  prices = prices1+ prices2
-  driver.find_element(By.XPATH, '//*[@id="search_keyword"]').clear()
-  sleep(0.2)
+#   # WebDriverWait(driver, 1000).until(
+#   #   EC.visibility_of_all_elements_located((By.XPATH, """//*[@id="listBodyDiv"]/div[2]/div[1]/div[3]/div[1]"""))
+#   # )
 
-  priceList= []
-  for p in prices :
-    str = (p.text).replace(',', '')
+#   sleep(0.3)
+#   first_product = driver.find_element(By.XPATH, '//*[@id="listBodyDiv"]/div[2]/div[1]/div[3]/div[1]/ul/li[1]/div/div[1]')
+#   first_product.click()
+#   sleep(0.3)
+#   #tab 이동
+#   driver.switch_to.window(driver.window_handles[1])
+#   btn_card_discount = driver.find_element(By.XPATH, '//*[@id="prod_pricecomp"]/div[1]/div/div[2]/div[2]/label')
+#   btn_card_discount.click()
+#   sleep(0.2)
+#   html = driver.page_source
+#   soup = BeautifulSoup(html, 'html.parser')
 
-    priceList.append(int(str))
+#   prices1 = soup.select('.tx--price')
+#   prices2 = soup.select('.col--price>a>em')
+#   prices = prices1+ prices2
+#   driver.find_element(By.XPATH, '//*[@id="search_keyword"]').clear()
+#   sleep(0.2)
+
+#   priceList= []
+#   for p in prices :
+#     str = (p.text).replace(',', '')
+
+#     priceList.append(int(str))
       
 
-  minPrice = min(priceList)
-  data[i].append(minPrice)
+#   minPrice = min(priceList)
+#   data[i].append(minPrice)
 
-  print(i+1)
-print(data)
+#   print(i+1)
+# print(data)
 
 
-f = open('example.csv', 'w', newline='')
-wr = csv.writer(f)
-wr.writerows(data)
-f.close()
+# f = open('example.csv', 'w', newline='')
+# wr = csv.writer(f)
+# wr.writerows(data)
+# f.close()
 
 
